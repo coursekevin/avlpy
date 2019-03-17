f648961f186e4d4e330e9b5404c3d06638fc06a7
Kevin Course <coursekevin@Kevins-MacBook-Pro.local>
2019-03-17 17:44:01 -0400

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md","r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = "avlpy",
+	version = "0.1",
+	author ="Kevin Course",
+	author_email = "kevin.course@mail.utoronto.ca",
+	description="A high-level avl wrapper in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/coursekevin/avlpy",
+    packages=setuptools.find_packages(),
+    install_requires=['avl']
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
