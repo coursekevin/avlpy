7fd89ac76e24935e21b0ea3a942f81c185744477
Kevin Course <coursekevin@Kevins-MacBook-Pro.local>
2019-03-17 18:25:53 -0400

    updated example.py

--- example/example.py
+++ example/example.py
@@ -30,14 +30,15 @@ fname2,proc_out2 = avlSess.get_eig_analysis('S')
 
 # read state matrices to python arrays
 A,B = avlpy.read_avl_sys_mat(fname2)
-
-# run custom command (navigate to oper menu then exit the menu)
-avlSess.custom_cmd(['oper','\n'])
+print("\nDynamic 'A' Matrix: " + str(A))
 
 # --------------------------------------------------------------------------------------------
 # READING AVL FILES
 # --------------------------------------------------------------------------------------------
 surfaces = avlpy.read_avl_file('wingus.avl')
+
+print("\nExample Surfaces File:")
+print(surfaces)
 
 # save surfaces to new surfaces.avl file
 avlpy.save_avl_file('surfaces.avl',surfaces)
